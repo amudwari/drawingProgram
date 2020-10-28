3af74997a9bbd73d3f3c8f0fd387c8e5cadd04e2
Apekshya Mudwari <41219304+amudwari@users.noreply.github.com>
2020-10-28 15:29:28 -0500

    UI context and undo button added

---
+++ UIContext.java
@@ -0,0 +1,7 @@
+public interface UIContext {
+  //  public abstract void drawCircle(Circle circle);
+  public abstract void draw(Line line);
+  public abstract void draw(Label label);
+  public abstract void draw(Quadrilateral quadrilateral );
+  public abstract void draw(Item item);
+}
